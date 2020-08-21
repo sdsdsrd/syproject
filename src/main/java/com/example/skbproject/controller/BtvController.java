a390edbfeee5eea79f12fab51b2bc7582d333827
sdsdsrd <1jmss@naver.com>
2020-08-22 00:18:32 +0900

    기능추가: REST API 호출 설정

--- src/main/java/com/example/skbproject/controller/BtvController.java
+++ src/main/java/com/example/skbproject/controller/BtvController.java
@@ -4,17 +4,25 @@ import com.example.skbproject.dto.BtvDto;
 import com.example.skbproject.dto.TopGenreDto;
 import com.example.skbproject.service.BtvService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class BtvController {
 
     @Autowired
     private BtvService service;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/log")
     public List<BtvDto> getLog() {
@@ -29,6 +37,11 @@ public class BtvController {
 
     @GetMapping("epsd/{stbId}/{now}")
     public List<String> getEpsdIdList(@PathVariable int stbId, @PathVariable String now) {
+        String url = "http://localhost:8080/genre/" + stbId + "/" + now;
+
+        List<TopGenreDto> topGenreDtos = restTemplate.getForObject(url, List.class);
+        System.out.println(topGenreDtos.size());
+
         return service.getEpsdIdList(stbId, now);
     }
 
